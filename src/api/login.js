55bfd0476a661e162e046825a58c724dfbae44e3
AloneBlackCat <2210439454@qq.com>
2024-01-11 17:12:51 +0800

    优化响应拦截

--- src/api/login.js
+++ src/api/login.js
@@ -31,7 +31,15 @@ export const GetUserinfo = () => {
 // 获取验证码
 export const GetValidateCode = () => {
   return request({
-    url: "/admin/system/index/generateValidateCode",
+    url: '/admin/system/index/generateValidateCode',
+    method: 'get'
+  })
+}
+
+// 退出登录
+export const Logout = () => {
+  return request({
+    url: '/admin/system/index/logout',
     method: 'get'
   })
 }
